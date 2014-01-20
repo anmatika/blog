db4132ea60cce3c1051538dbd9f3059a27a6d530
anmatika <4ntti.matikainen@gmail.com>
2014-01-20 07:51:38 +0200

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 1 commit. # # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # 	modified:   .idea/workspace.xml 	modified:   Gemfile 	modified:   Gemfile.lock #

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,6 @@
 (function($){
     $(document).ready(function(){
-        $(table).dataTable();
+        $('table').dataTable();
         $('.list-group a').hover(function(){
             $(this).find('i').toggleClass('fa-2x');
         });
