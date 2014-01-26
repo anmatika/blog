e08ac86841aef2c6ee0046ee8604b2b7a607a146
anmatika <4ntti.matikainen@gmail.com>
2014-01-26 09:42:51 +0200

    modified:   .idea/workspace.xml 	modified:   app/controllers/users_controller.rb 	new file:   app/views/layouts/_log_in.html.erb 	modified:   app/views/layouts/_top_nav.html.erb 	modified:   app/views/users/show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      redirect_to user_path(@user), :notice => "signed up!"
+      #redirect_to user_path(@user), :notice => "signed up!"
+      redirect_to log_in_path, :notice => "sign up successful, you may now log in."
     else
      # render new
       flash[:error] = "User save failed"
